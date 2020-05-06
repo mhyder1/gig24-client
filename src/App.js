46bc812b5485268c716270e4c9d96b9d6ba3b84d
Anar Chalk <annachak9@gmail.com>
2020-05-06 01:58:07 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+
+    return <div className="App">
+      <h1>Solo parenting</h1>
+      <p>The Solo Parenting app is built to help single parents connect and support each other. Assist with day to day activities by seamlessly toggling between schedules. Our community of helpers provide practica support such as babysitting, transporting, and dogwalking and plenty more. Sign up and join a virtual village like no other </p>
+    </div>;
+  }
 }
 
 export default App;
