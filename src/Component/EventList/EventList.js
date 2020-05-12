f6306581919b3c4deb744ba0fafbce80d8f41596
Anar Chalk <annachak9@gmail.com>
2020-05-12 01:40:37 -0400

    added date and time in eventlist

--- src/Component/EventList/EventList.js
+++ src/Component/EventList/EventList.js
@@ -1,44 +1,38 @@
 import React, { Component } from "react";
 import AppContext from "../Context/AppContext";
-import { Link } from "react-router-dom";
-import TokenService from "../../services/token-service";
+import {Link} from 'react-router-dom'
+import { NiceDate } from '../Utils/Utils'
+import TokenService from '../../services/token-service'
 import "./eventList.css";
 
 export default class EventList extends Component {
   static contextType = AppContext;
 
   render() {
-    const type = this.props.match.url.split("/")[1];
-    const { events } = this.context;
-    const token = TokenService.hasAuthToken()
-      ? TokenService.readJwtToken()
-      : { user_id: "" };
-    //console.log(events);
-    const eventList = events.filter((event) => event.type === type);
+    const type = this.props.match.url.split('/')[1]
+    const token = TokenService.hasAuthToken() ? TokenService.readJwtToken() : {user_id:''}
+    const { events } = this.context
+    const eventList = events.filter(event => event.type === type)
     
     return (
       <>
-        <button style={{ backgroundColor: "green", color: "white" }}>
-          <Link
-            style={{ textDecoration: "none", color: "white" }}
-            to="/add-events"
-          >
-            Create your event
-          </Link>
-        </button>
-        
+        <button style={{backgroundColor:'green', color:'white'}}><Link style={{textDecoration:'none', color:'white'}} to='/add-events'>Create your event</Link></button>
         <h3>Current events</h3>
-        <ul className="ev-list">
+        <ul className="event-list">
           {eventList.map((event, id) => (
             <li key={id}>
               <p className="bold">{event.parent_name}</p>
               <p className="bold">{event.title}</p>
+              {/* <p>{event.time_of_event}</p> */}
+              <NiceDate
+                date={new Date(event.time_of_event)}
+              />
               <p className="bold">Description</p>
               <p>{event.description}</p>
               <p className="bold">Address</p>
-              <p className='bdata'>{event.address}</p>
+              <p>{event.address}</p>
               {(event.author !== token.user_id) &&
-                <button id='join-btn'>
+                <button>
                   <Link 
                     style={{textDecoration:'none', color:'black'}} 
                     to={{
@@ -64,7 +58,7 @@ export default class EventList extends Component {
                 </button>
               }
               <hr />
-              <br />
+              <br/>
             </li>
           ))}
         </ul>
