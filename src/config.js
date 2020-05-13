1f0f07a2f708db314905d6c6dac10e467b2ec6cf
Anar Chalk <annachak9@gmail.com>
2020-05-13 16:57:44 -0400

    config

--- src/config.js
+++ src/config.js
@@ -1,8 +1,7 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: 'https://mysterious-lowlands-90540.herokuapp.com/api',
     APPID:'plX8VD1DPX0N',
     SEARCH_KEY:'db86cffac7f634ffb4975e0cc879b169',
     TOKEN_KEY:  '71448d7d-be5b-4dba-8157-4c0d21fb34e6'
 }
 
-//https://mysterious-lowlands-90540.herokuapp.com/api
